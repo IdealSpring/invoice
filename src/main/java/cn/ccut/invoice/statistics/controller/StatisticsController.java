7b4e2d1f805a453c6cb4d53a1a9bec8d37c6f5c6
IdealSpring <zhipeng_Tong@126.com>
2018-01-24 15:20:30 +0800

    To complete! d

--- src/main/java/cn/ccut/invoice/statistics/controller/StatisticsController.java
+++ src/main/java/cn/ccut/invoice/statistics/controller/StatisticsController.java
@@ -1,19 +1,14 @@
 package cn.ccut.invoice.statistics.controller;
 
-import cn.ccut.invoice.statistics.service.InvoiceService;
+import cn.ccut.invoice.statistics.service.StatisticsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 
-/**
- * statistics控制
- */
 @Controller
 @RequestMapping("/statistics")
 public class StatisticsController {
-    @Resource(name = "InvoiceServiceImpl")
-    private InvoiceService invoiceService;
-
-
+    @Resource(name = "statisticsServiceImpl")
+    private StatisticsService statisticsService;
 }
